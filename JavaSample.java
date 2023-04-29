93194a1a211d66f8c36d5db9332784a559fe0769
reshank <reshankreddy7@gmail.com>
2023-04-29 02:12:26 -0500

    third commit multiply added

--- JavaSample.java
+++ JavaSample.java
@@ -4,11 +4,18 @@ class JavaSample
 	{
 		System.out.println("Hello");
 		System.out.println("Sum="+sum(10,20));
+		System.out.println("Product="+multiply(10,20));
 	}
 
 	public int sum(int a, int b)
 	{
 		int c= a+b;
+		return c;
+	}
+
+	public multiply(int a, int b)
+	{
+		int c=a*b;
 		return c;
 	}
 }
