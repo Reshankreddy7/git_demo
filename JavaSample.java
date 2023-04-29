f249e78067ce0b88b78072b811a66ab6cd893691
reshank <reshankreddy7@gmail.com>
2023-04-29 02:07:46 -0500

    second commit SUM() method is added

--- JavaSample.java
+++ JavaSample.java
@@ -3,5 +3,12 @@ class JavaSample
 	public static void main(String args[])
 	{
 		System.out.println("Hello");
+		System.out.println("Sum="+sum(10,20));
+	}
+
+	public int sum(int a, int b)
+	{
+		int c= a+b;
+		return c;
 	}
 }
