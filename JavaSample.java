3491248d85cb8f23c8b2e7fdbf1e20e3a608dbfa
git hub <reshankreddy7@gmal.com>
2023-05-04 13:20:55 -0500

    changes added

--- JavaSample.java
+++ JavaSample.java
@@ -3,16 +3,16 @@ class JavaSample
 	public static void main(String args[])
 	{
 		System.out.println("Hello");
-		System.out.println("Sum="+sum(10,20));
+		System.out.println("Sum="+sum(10,20,30));
 		System.out.println("Product="+multiply(10,20));
 		System.out.println("Division="+divide(20,10));
 		System.out.println("Division="+sum(20,10,40));
 
 	}
 
-	public int sum(int a, int b)
+	public int sum(int a, int b, int c)
 	{
-		int c= a+b;
+		int c= a+b+c;
 		return c;
 	}
 
